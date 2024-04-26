a28d385819ebb95c498a2437edeabd1d274fdf0e
ashwin-raguraj <ashwinraguraj@gmail.com>
2024-04-26 16:59:36 +0530

    added notifications

--- SmartMeds/frontend/src/Components/Navbar3.js
+++ SmartMeds/frontend/src/Components/Navbar3.js
@@ -5,10 +5,12 @@ import axios from 'axios';
 import { ImMenu3 } from 'react-icons/im';
 import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faBell,faHeartPulse } from '@fortawesome/free-solid-svg-icons';
 const Navbar3 = () => {
 
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isDropdownOpen1, setIsDropdownOpen1] = useState(false);
+    const [notifications, setNotifications] = useState([]);
     const handleSignOut=async ()=>{
         try {
             const response = await axios.get('http://127.0.0.1:8000/doctor/signout');//API for log out
@@ -18,9 +20,12 @@ const Navbar3 = () => {
             console.error(error);
           }
       };
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
+      const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+      };
+      const toggleDropdown1 = () => {
+        setIsDropdownOpen1(!isDropdownOpen1);
+      };
   const [doctorInfo, setDoctorInfo] = useState([]);
   const fetchDashboardData = async () => {
     try {
@@ -34,9 +39,19 @@ const Navbar3 = () => {
       console.log(error);
     }
   };
-
+  const fetchNotifications = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000/api/notifications/');
+      if (response && response.data) {
+        setNotifications(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
     fetchDashboardData();
+    fetchNotifications();
   }, []);
   return (
     <nav className='navbar'>
@@ -45,10 +60,50 @@ const Navbar3 = () => {
             <div >SmartMeds Connect</div>
         </div> 
         <ul>
-            <li className='nav-item'>
-                {/* <Link to='/DocDashboard'>Dashboard</Link> */}
-                <FontAwesomeIcon icon={faBell} size="xl" style={{color: "#1c4ee6",marginLeft: "-30px"}} />
-
+        <li className='nav-item'>
+                {/* <Link to='/Dashboard'>Dashboard</Link> */}
+                <div>
+      {notifications.length > 0 ? (
+        <FontAwesomeIcon
+          onClick={toggleDropdown1}
+          icon={faBell} shake// or any other icon you want to use for notifications
+          size="xl"
+          style={{ color: "c84646", marginLeft: "-30px" }}
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faBell} 
+          size="xl"
+          style={{ color: "grey", marginLeft: "-30px" }}
+        />
+      )}
+      {isDropdownOpen1 && (
+        <div className="dropdown1">
+          {notifications.length > 0 ? (
+            <ul className="dropdown1-menu d-flex flex-column">
+                <p style={{ fontSize: '26px', color: '' }}><FontAwesomeIcon icon={faHeartPulse} style={{color: "#cf3a3a",}} /><strong> Missed Medications</strong></p>
+              {notifications.map(notification => (
+                <li key={notification.id}>
+                  <div className="notification-item">
+                    
+                    <p><strong>Patient ID:</strong> {notification.patient_id}</p>
+                    
+                    <p><strong>Date:</strong> {notification.date}</p>
+                    <p><strong>Medicine:</strong> {notification.medicine}</p>
+                    <p><strong>Time:</strong> {notification.time}</p>
+                    <p><strong>Quantity:</strong> {notification.qty}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No notifications to show</p>
+          )}
+        </div>
+      )}
+    </div>
+                
+                
             </li>
             {doctorInfo.map((doctor, index) => (
                         <div key={index}>
